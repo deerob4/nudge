5aab79d87e9e140b71cf672d092beaf69295b854
Dee Roberts <deerob4@gmail.com>
2015-12-02 15:59:10 +0000

    Add fields to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->date('dob');
+            $table->char('gender', 1);
+            $table->string('sexuality');
             $table->rememberToken();
             $table->timestamps();
         });
