695fb789891ce8155c513fbc3836c11560d386e9
Dee Roberts <deerob4@gmail.com>
2015-12-16 19:18:50 +0000

    Update email template

--- resources/views/emails/new_nudge.blade.php
+++ resources/views/emails/new_nudge.blade.php
@@ -1,5 +1,4 @@
 <h1>You've been nudged!</h1>
 <h2>Hi {{ $user->name }},</h2>
-<img src={{ $nudger->photo_url }} alt={{ $nudger->name }} />
-<p>It looks like {{ $nudger->name }} has taken an interest in you. Visit {{ $nudger->facebook_link }} to find out more!</p>
+<p>It looks like someone has taken an interest in you. Visit <a href={{ $nudger->facebook_link }}>here</a> to find out more!</p>
 <h3>Thanks, <br> The nudj Team</h3>
